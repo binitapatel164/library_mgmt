07624e25ed442c87b7ef4427ee45d7837f986afd
binitapatel164 <103460459+binitapatel164@users.noreply.github.com>
2024-02-26 18:39:54 -0600

    fixed issues5

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ import os
 
 # Get the current directory of this script
 current_dir = os.path.dirname(os.path.abspath(__file__))
-template_dir = os.path.join(current_dir, 'src/templates')  # Path to templates folder
+template_dir = os.path.join(current_dir, 'templates')  # Path to templates folder
 print("Current Directory:", current_dir)
 print("Template Directory:", template_dir)
 

da27de8f0745ba8ebbfc03c41e3212dad64c801a
binitapatel164 <103460459+binitapatel164@users.noreply.github.com>
2024-02-26 18:15:35 -0600

    added flaskapp changes

--- src/app.py
+++ src/app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template
+import os
 
-app = Flask(__name__)
+# Get the current directory of this script
+current_dir = os.path.dirname(os.path.abspath(__file__))
+template_dir = os.path.join(current_dir, 'src/templates')  # Path to templates folder
+print("Current Directory:", current_dir)
+print("Template Directory:", template_dir)
+
+app = Flask(__name__, template_folder=template_dir)
 
 @app.route('/')
 def index():

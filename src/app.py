f10af0c3e42468eb9d1b5ba29b9d7e03feababf1
binitapatel164 <103460459+binitapatel164@users.noreply.github.com>
2024-02-26 18:44:45 -0600

    fixed issues6

--- src/app.py
+++ src/app.py
@@ -1,17 +1,11 @@
-from flask import Flask, render_template
-import os
+from flask import Flask
 
-# Get the current directory of this script
-current_dir = os.path.dirname(os.path.abspath(__file__))
-template_dir = os.path.join(current_dir, 'templates')  # Path to templates folder
-print("Current Directory:", current_dir)
-print("Template Directory:", template_dir)
+app = Flask(__name__)
 
-app = Flask(__name__, template_folder=template_dir)
-
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return "Hello, world!"
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run()
